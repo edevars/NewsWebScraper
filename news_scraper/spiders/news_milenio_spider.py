295fc21cc519ebe33519ac247da0222d7172fbc2
edevars <e.devars.dev@gmail.com>
2021-03-21 22:46:39 -0600

    :sparkles: Scraping milenio politic news

--- news_scraper/spiders/news_milenio_spider.py
+++ news_scraper/spiders/news_milenio_spider.py
@@ -1,19 +1,44 @@
 import scrapy
 
-NAME= 'Milenio'
+NAME = 'Milenio'
 BASE_URL = 'https://www.milenio.com'
 TARGET_URL = 'https://www.milenio.com/politica'
 ARTICLE_LINK = '//div[@class="title"]/a/@href'
+
 
 class NewsSpider(scrapy.Spider):
     name = 'news_milenio'
     start_urls = [TARGET_URL]
 
+    custom_settings = {
+        'FEED_URI': 'milenio.csv',
+        'FEED_FORMAT': 'csv',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+
     def parse(self, response):
-        print("*" * 10)
-        print("\n" * 3)
-        articles = response.xpath(ARTICLE_LINK).getall()
-        for article in articles:
-            print(BASE_URL + article)
-        print("\n" * 3)
-        print("*" * 10)
+        base_links = response.xpath(ARTICLE_LINK).getall()
+        articles_links = [BASE_URL + link for link in base_links]
+        for link in articles_links:
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': link})
+
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="title"]/text()').get()
+        author = response.xpath(
+            '//div[@class="nd-content-body"]/span[@class="author"]/text()').get()
+        location = response.xpath(
+            '//div[@class="content-date"]/span[@class="location"]/text()').get()
+        datetime = response.xpath('//time/@datetime').get()
+        article_paragraphs = response.xpath(
+            '//div[@id="content-body"]/p/descendant-or-self::text()').getall()
+        article_text = ''.join(article_paragraphs)
+
+        yield {
+            'url': link,
+            'title': title,
+            'author': author,
+            'location': location,
+            'date_time': datetime,
+            'article_text': article_text
+        }
